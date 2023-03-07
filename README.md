5dbc99b75ad623be9f7f799df6787dbcdff93106
Lucas Campolim <lucascampolim@protonmail.com>
2023-03-07 08:34:46 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Instruções
+
+1 - Você precisa ter em sua máquina o conjunto de ferramentas GNU, o MinGW.
+Você pode obter a versão mais estável no link: https://github.com/niXman/mingw-builds-binaries/releases
+Recomendo baixar a versão: 12.2.0-rt_v10-rev2/x86_64-12.2.0-release-win32-seh-ucrt-rt_v10-rev2.7z
+
+2 - Extraia a pasta do MinGW no diretório raiz do Windows, no meu caso é o C:\
+
+3 - Copie a pasta sqlite-libs no diretório raiz do Windows, no meu caso é o C:\
+
+4 - Configure as variáveis de ambiente para o MinGW e o SQLite.
+Adicione na PATH os caminhos:
+- C:\mingw64\bin
+- C:\sqlite-libs
+
+5 - Instale o Visual Stdio Code para compilar e executar o código dentro da IDE.
+Você pode obter a versão mais estável no link: https://visualstudio.microsoft.com/pt-br/downloads/
+
+6 - Ao abrir o código main.c pelo Visual Studio Code ele irá te sugerir uma extensão para programas feitos em C, você precisará instalar ela.
+
+7 - Na linha 2 do código especifique o caminho da biblioteca do SQLite.
+    Na linha 45 especifique o caminho do banco de dados.
+
+8 - Caso faça uma alteração no código você pode recompilar pelo comando:
+gcc main.c sqlite3.o -Ic:\sqlite -o main.exe
+
+9 - Para rodar o programa use o comando:
+.\main.exe
---
+++ code/main.c
@@ -0,0 +1,167 @@
+// Bibliotecas.
+#include "C:\sqlite\sqlite3.h"
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Conta o números de retornos da query.
+int rows;
+// Função de callback para exibir retornos da query.
+/**
+ * callback: É uma função que é chamada como argumento de uma outra função.
+ * *NotUsed: Parâmentro padrão da documentação do SQlite3, ponteiro vazio.
+ * argc: Contagem do número de atributos do registro.
+ * **argv: Conjunto de dados em formado de ponteiro do ponteiro. Quando você junta 2 ponteiros simulo uma matriz.
+ * **azColName: O mesmo de argv, mas invés de conter os dados da query, vai conter o nome dos atributos do registro.
+ * ?: É um inlineif, um if else que cabe em uma linha só. Se for true retorna o primeiro campo, se for falso retorna o segundo campo.
+*/
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    // Incrementa a contagem do número de linhas retornadas pela query.
+   rows++;
+   // Percorre os atributos retornados pela query para exibir os dados do registro.
+   for(int i = 0; i < argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+// callback2: Faz a contagem de registros retornadas pela query. Esse callback apenas exibe os dados.
+static int callback2(void *NotUsed, int argc, char **argv, char **azColName) {
+    // Incrementando para contar os registros da query.
+   rows++;
+   return 0;
+}
+// Chamada principal.
+int main()
+{
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    // Armazena uma mensagem de erro que eu não quis exibir.
+    char *zErrMsg = 0;
+    // Ponteiro que referencia o BD que estou utilizando.
+    sqlite3* DB;
+    // Váriável noobinha para saber se o banco vai dar liga.
+    int erro = 0;
+    erro = sqlite3_open("C:\\ProjetoC#\\DB\\banco_estudantes.db", &DB);
+    if (erro) {
+        printf("Erro ao abrir o BD! \n");
+        return -1;
+    }
+    int sair, op;
+    // Variável de controle para o while não rodar infinitamente.
+    sair = 0;
+    int ID;
+    char sql[250];
+    char nome[50], nome_pai[50], endereco[50];
+    while (!sair)
+    {
+        printf("\nEscolha uma opção: \n");
+        printf("1) Listar todos os alunos \n");
+        printf("2) Buscar aluno pelo ID \n");
+        printf("3) Inserir novo aluno \n");
+        printf("4) Remover um aluno \n");
+        printf("5) Atualizar dados do aluno \n");
+        printf("6) Sair \n\n");
+        printf(">> ");
+        // &: scanf lê um decimal digitado pelo usuário e salva no endereço de memória de op.
+        scanf("%d", &op);
+        switch (op)
+        {
+            case 1:
+                // strcopy é uma função para copiar o conteúdo de texto para dentro de uma variável.
+                // Está jogando o meu texto de select para dentro de sql.
+                printf("Alunos listados: \n\n");
+                strcpy(sql, "SELECT * from Students");
+                // Executa a SQL no banco.
+                sqlite3_exec(DB, sql, callback, 0, &zErrMsg);
+                break;
+            case 2:
+                // rows é resetado antes de contar quantos itens o select achou.
+                rows = 0;
+                printf("\nDigite o ID: ");
+                scanf("%i", &ID);
+                printf("\nInformações do ID: \n");
+                // sprintf junta dentro de SQL uma nova query formatada.
+                // sprintf formata uma string para dentro de uma variável.
+                sprintf(sql, "SELECT * from Students WHERE ID = %i", ID);
+                sqlite3_exec(DB, sql, callback, 0, &zErrMsg);
+                // Se rows for 0, siginifica que o usuário fez bosta.
+                if (rows == 0)
+                {
+                    printf("\nNenhum usuário com esse ID! \n");
+                }
+                break;
+            case 3:
+                // getchar pega um caractere que está em um buffer de entrada porque isso atrapalharia a execução do case.
+                getchar();
+                printf("\nDigite o nome do aluno: ");
+                // gets tem uma leitura mais completa, evita ter que ficar formatando.
+                gets(nome);
+                printf("Digite o nome do pai do aluno: ");
+                gets(nome_pai);
+                printf("Digite o endereço do aluno: ");
+                gets(endereco);
+                // Insere uma nova entrada no BD.
+                // \"%s\": É uma string que vai ficar entre aspas dentro da query. É uma formatação.
+                sprintf(sql, "INSERT INTO Students (Name, FatherName, Address) VALUES (\"%s\", \"%s\", \"%s\"); ", nome, nome_pai, endereco);
+                sqlite3_exec(DB, sql, callback, 0, &zErrMsg);
+                printf("O aluno foi inserido com sucesso! \n");
+                break;
+            case 4:
+                rows = 0;
+                getchar();
+                printf("\nDigite o ID do aluno que deseja remover: ");
+                scanf("%i", &ID);
+                // Para remover verificar se o ID existe.
+                sprintf(sql, "SELECT * from Students WHERE ID = %i", ID);
+                sqlite3_exec(DB, sql, callback2, 0, &zErrMsg);
+                // Se o usuário digitar um registro inválido apenas exige a mensagem de erro.
+                if (rows == 0)
+                {
+                    printf("\nNenhum usuário com esse ID! \n");
+                } else {
+                    sprintf(sql, "DELETE from Students WHERE ID = %i", ID);
+                    sqlite3_exec(DB, sql, 0, 0, &zErrMsg);
+                    printf("Registro apagado com sucesso! \n\n");
+                }
+                break;
+            case 5:
+                rows = 0;
+                getchar();
+                printf("\nDigite o ID do aluno que deseja atualizar: ");
+                scanf("%i", &ID);
+                // É preciso selecionar para atualizar.
+                sprintf(sql, "SELECT * from Students WHERE ID = %i", ID);
+                sqlite3_exec(DB, sql, callback2, 0, &zErrMsg);
+                if (rows == 0)
+                {
+                    printf("\nNenhum usuário com esse ID! \n");
+                } else {
+                    getchar();
+                    printf("\nDigite o nome do aluno: ");
+                    gets(nome);
+                    printf("Digite o nome do pai do aluno: ");
+                    gets(nome_pai);
+                    printf("Digite o endereço do aluno: ");
+                    gets(endereco);
+                    // Formata a query de update e faz o update.
+                    sprintf(sql, "UPDATE Students set Name=\"%s\", FatherName=\"%s\", Address=\"%s\" WHERE ID = %i", nome, nome_pai, endereco, ID);
+                    sqlite3_exec(DB, sql, 0, 0, &zErrMsg);
+                    printf("Registro atualizado com sucesso! \n\n");
+                }
+                break;
+            case 6: 
+                sair = 1;
+                printf("Você saiu do programa! \n");
+                return 0;
+                break;
+            default:
+                printf("Opção inválida! \n");
+        }
+        printf("\n");
+        system("pause");
+    }
+    // Fecha o BD.
+    sqlite3_close(DB);
+    return 0;
+}
